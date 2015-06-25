211a83943ef5b766cbbae949e1e7c9ce15b1ba41
unknown <p1414331@IUTDOUAB2PC17.univ-lyon1.fr>
2015-06-25 08:59:13 +0200

    BasesCSharpTests 	DéfinirUneClasseRealisantUneMultiplication 		creation nouvelle classe + 2 methodes 			test ok

---
+++ FinalTest/Multiplication.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace FinalTest.Tests
+{
+    public class Multiplication
+    {
+        public bool PeutCalculer(string sCalc)
+        {
+            bool bRet = true;
+
+            if (sCalc.Contains("+"))
+            {
+                bRet = bRet && false;
+            }
+            else if (sCalc.Contains("*"))
+            {
+                bRet = bRet && true;
+            }
+
+            return bRet;
+        }
+
+        public int Calculer(string sCalc)
+        {
+            int iRet = 1;
+
+            string[] split = sCalc.Split(new Char[] { '*' });
+
+            foreach (var s in split)
+            {
+                iRet = iRet * int.Parse(s);
+            }
+
+            return iRet;
+        }
+    }
+}
---
+++ FinalTest/TypeReference.cs
@@ -0,0 +1,17 @@
+﻿namespace FinalTest
+{
+    public class TypeReference
+    {
+        public int i;
+
+        public TypeReference(int i)
+        {
+            this.i = i;
+        }
+
+        public bool Equals(TypeReference obj)
+        {
+            return obj.i == this.i;
+        }
+    }
+}
