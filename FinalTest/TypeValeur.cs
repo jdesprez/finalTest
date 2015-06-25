0653ef3b0d571fd6d1af341011b9ff5db6eb088a
unknown <p1414331@IUTDOUAB2PC17.univ-lyon1.fr>
2015-06-25 08:39:07 +0200

    BasesCSharpTests 	DefinirUnTypeValeur 		creation TypeValeur de type struct 			test ok

---
+++ FinalTest/TypeValeur.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalTest
+{
+    public struct TypeValeur
+    {
+        private int i;
+
+        public TypeValeur(int i)
+        {
+            this.i = i;
+        }
+    }
+}
